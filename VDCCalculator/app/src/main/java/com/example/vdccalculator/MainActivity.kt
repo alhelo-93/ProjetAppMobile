6f2c108fb68c08dda9f58c7c500219ca997499dd
SOARES Miguel <Miguel.SOARES@cpnv.ch>
2020-11-20 14:36:39 +0100

    Update MainActivity.kt

--- VDCCalculator/app/src/main/java/com/example/vdccalculator/MainActivity.kt
+++ VDCCalculator/app/src/main/java/com/example/vdccalculator/MainActivity.kt
@@ -8,45 +8,39 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import java.math.BigInteger
-
-
+import kotlin.math.absoluteValue
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // get refence to button
-        var btn_calculate = findViewById<View>(R.id.button2) as Button
-        var PdNumber = findViewById<View>(R.id.editTextNumber) as EditText
-        var ChNumber = findViewById<View>(R.id.editTextNumber2) as EditText
-        var PlNumber = findViewById<View>(R.id.editTextNumber3) as EditText
-        var ChfView = findViewById<View>(R.id.textView4) as TextView
+
+        var btn_calculate = findViewById(R.id.button2) as Button
 
         btn_calculate.setOnClickListener(View.OnClickListener {
-            var PdNumberint = 0
-            val PdNumber = PdNumberint.toInt()
+            var PdNumber = findViewById(R.id.editTextNumber) as EditText
+            var ChNumber = findViewById(R.id.editTextNumber2) as EditText
+            var PlNumber = findViewById(R.id.editTextNumber3) as EditText
+            var ChfView = findViewById<View>(R.id.textView4) as TextView
+
+            val pdNumberstr:String = PdNumber.text.toString()
+            val pdNumberInt = pdNumberstr.toInt()
+            val chNumberstr:String = ChNumber.text.toString()
+            var chNumberInt = chNumberstr.toInt()
+            val plNumbersrt:String = PlNumber.text.toString()
+            var plNumberInt = plNumbersrt.toInt()
             var num2 = 0
-            var ChNumberInt = 0
-            val ChNumber = ChNumberInt.toInt()
-            var PlNumberInt = 0
-            val PlNumber = PlNumberInt.toInt()
+
+            var pdNumberTot= pdNumberInt * 0.15
+            var chNumberTot = chNumberInt*2
 
 
-            var num1 = PdNumberint * 0.15
-
-            if (ChNumber >200){
-                 num2 = ChNumber  * 3
-            }else {
-                 num2 = ChNumber * 2
-            }
-
-            var restot = num1 + num2
-            if (PlNumber < 125){
-
-            }
-            ChfView.text="La taxe de palques sera de"+restot.text.toString()
+           var restot = chNumberTot + pdNumberTot + 40
+            ChfView.text="La taxe de palques sera de"+restot
         })
 
 

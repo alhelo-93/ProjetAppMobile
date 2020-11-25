95c19a22fd72e649cae798d1fd942862fcd3cf77
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2020-11-25 09:07:29 +0100

    Update MainActivity.kt
    add the constants

--- VDCCalculator/app/src/main/java/com/example/vdccalculator/MainActivity.kt
+++ VDCCalculator/app/src/main/java/com/example/vdccalculator/MainActivity.kt
@@ -41,31 +41,47 @@ class MainActivity : AppCompatActivity() {
             var plNumberInt = plNumbersrt.toInt()
             var num2 = 0
             var chNumberTot = 0
+            // Here constant
+            // The Kg price of cars wight
+            var Kgprice : Double = 0.15
+            // The engine power  Measured on  KW
+            var KwMeasure : Int = 100
+            // The price if less than KwMeasure
+            var lowerPrice  : Int = 2
+            // The price if less than KwMeasure
+            var higherPrice : Int = 3
+            // The pollution Measured By Co2/KM
+            var CO2Measure : Int = 125
+            // the base taxes to all vehicles
+            var BasicTaxes : Int = 40
+            // Green Taxes
+            var GreenRebate : Double = 0.75
 
-            var pdNumberTot = pdNumberInt * 0.15
+
+            var pdNumberTot = pdNumberInt * Kgprice
             //If we have 100 kW or less
-            if (chNumberInt <= 100) {
-                 chNumberTot = chNumberInt * 2
+            if (chNumberInt <= KwMeasure) {
+                 chNumberTot = chNumberInt * lowerPrice
             }
             //If we have more than 100 KW
             else {
                 // we substract 100 from the total kw
-                var chMore100 = chNumberInt - 100
+                var chMore100 = chNumberInt - KwMeasure
                 //the rest we multiply by 3
-                var chMore100Cl = chMore100 * 3
+                var chMore100Cl = chMore100 * higherPrice
                 //the 100 we multiply by 2
-                var ch100 = 100 * 2
+                var ch100 = KwMeasure * lowerPrice
                 //We add all together
                  chNumberTot = chMore100Cl + ch100
             }
-            //If the car polutes less than 125G of Co2/Km or electric/hybrid car
-            if (plNumberInt < 125) {
+            //If the car pollutes less than 125G of Co2/Km or electric/hybrid car
+            if (plNumberInt < CO2Measure) {
                 //we take the total power in KW  price and we add the total weigth price
                 var restot = chNumberTot + pdNumberTot
                 //We make the 75% of the total price if the car is "Green"
-                var restotrab = restot - restot * 75 / 100
+                var restotrab = restot - (restot * GreenRebate)
                 //we add the base taxe to all vehicles
-                var restotrabfin = restotrab + 40
+                var restotrabfin = restotrab + BasicTaxes
                 //show the total result price
                 ChfView.text = "La taxe de palques sera de " + restotrabfin
             }
@@ -74,7 +90,7 @@ class MainActivity : AppCompatActivity() {
                 //we take the total power in KW  price and we add the total weigth price
                 var restot = chNumberTot + pdNumberTot
                 //we add the base taxe to all vehicles
-                var restrotfin = restot + 40
+                var restrotfin = restot + BasicTaxes
                 //show the total result price
                 ChfView.text = "La taxe de palques sera de " + restrotfin
             }

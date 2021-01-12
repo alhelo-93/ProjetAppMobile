5ce94aabdb354dd9054cd68feecd8fea1632c37f
SOARES Miguel <Miguel.SOARES@cpnv.ch>
2021-01-12 14:15:29 +0100

    broken app file

---
+++ Broken APP/ProjetAppMonile/app/src/main/java/com/example/vdccalculatorv2/ui/Assurance/AssuranceViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.vdccalculatorv2.ui.Assurance
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AssuranceViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}

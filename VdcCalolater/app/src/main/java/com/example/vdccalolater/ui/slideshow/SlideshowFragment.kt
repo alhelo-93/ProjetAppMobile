0ae055c8e3cc73d8b081698143730cf0d05ad6f2
SOARES Miguel <Miguel.SOARES@cpnv.ch>
2021-01-15 09:09:08 +0100

    new code

--- VdcCalolater/app/src/main/java/com/example/vdccalolater/ui/slideshow/SlideshowFragment.kt
+++ VdcCalolater/app/src/main/java/com/example/vdccalolater/ui/slideshow/SlideshowFragment.kt
@@ -22,10 +22,6 @@ class SlideshowFragment : Fragment() {
         slideshowViewModel =
                 ViewModelProvider(this).get(SlideshowViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }

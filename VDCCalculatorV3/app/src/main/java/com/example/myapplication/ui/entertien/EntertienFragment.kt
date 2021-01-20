f3257b4b4f45e43ea9f1a4f23b80ff796a35fda7
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2021-01-20 09:18:59 +0100

    refactor entertien page's name

--- VDCCalculatorV3/app/src/main/java/com/example/myapplication/ui/entertien/EntertienFragment.kt
+++ VDCCalculatorV3/app/src/main/java/com/example/myapplication/ui/entertien/EntertienFragment.kt
@@ -1,27 +1,25 @@
-package com.example.myapplication.ui.slideshow
+package com.example.myapplication.ui.entertien
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
 
-class SlideshowFragment : Fragment() {
+class EntertienFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+    private lateinit var entertienViewModel: EntertienViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-                ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        entertienViewModel =
+                ViewModelProvider(this).get(EntertienViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_entertien, container, false)
 
         return root
     }
--- VDCCalculatorV3/app/src/main/java/com/example/myapplication/ui/entertien/EntertienViewModel.kt
+++ VDCCalculatorV3/app/src/main/java/com/example/myapplication/ui/entertien/EntertienViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.myapplication.ui.slideshow
+package com.example.myapplication.ui.entertien
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class EntertienViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"

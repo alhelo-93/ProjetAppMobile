8a46cbaea1644266633576214c73876d6ca75ce7
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2021-01-20 09:20:33 +0100

    create new frangemnt

---
+++ VDCCalculatorV3/app/src/main/java/com/example/myapplication/ui/plaque/PlaqueFragment.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.ui.plaque
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.R
+
+class PlaqueFragment : Fragment() {
+
+    private lateinit var plaqueViewModel: PlaqueViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        plaqueViewModel =
+                ViewModelProvider(this).get(PlaqueViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_plaque, container, false)
+
+        return root
+    }
+}

a877910a6af6e8ca8f98a0825f033bacee9522a8
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2021-01-21 16:55:51 +0100

    add data base

--- VDCCalculatorV3/app/src/main/assets/vdc_dbv1.sql
+++ VDCCalculatorV3/app/src/main/assets/vdc_dbv1.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `assures`
 --
 
-DROP TABLE IF EXISTS `assures`;
+DROP TABLE IF EXISTS assures;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assures` (
